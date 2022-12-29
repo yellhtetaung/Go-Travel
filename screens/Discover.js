c59703803bcfcd5ae7149591eceedab1af159e1f
yellhtetaung <yairhtet248.yha@gmail.com>
2022-12-29 18:40:21 +0630

    updated

--- screens/Discover.js
+++ screens/Discover.js
@@ -6,19 +6,22 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { Attractions, Avatar, Hotels, Restaurants } from "../assets";
+import { Attractions, Avatar, Hotels, NotFound, Restaurants } from "../assets";
 import MenuContainer from "../components/MenuContainer";
-
 import { FontAwesome } from "@expo/vector-icons";
+
 import ItemCardContainer from "../components/ItemCardContainer";
 
 const Discover = () => {
   const navigation = useNavigation();
 
   const [type, setType] = useState("restaruants");
+  const [isLoading, setIsLoading] = useState(false);
+  const [mainData, setMainData] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -57,66 +60,92 @@ const Discover = () => {
       </View>
 
       {/* Menu Container */}
-      <ScrollView>
-        <View className="flex-row items-center justify-between px-8 mt-8">
-          <MenuContainer
-            key={"101"}
-            title="Hotels"
-            imageSrc={Hotels}
-            type={type}
-            setType={setType}
-          />
-
-          <MenuContainer
-            key={"102"}
-            title="Attractions"
-            imageSrc={Attractions}
-            type={type}
-            setType={setType}
-          />
-
-          <MenuContainer
-            key={"103"}
-            title="Restaurants"
-            imageSrc={Restaurants}
-            type={type}
-            setType={setType}
-          />
+      {isLoading ? (
+        <View className="flex-1 items-center justify-center">
+          <ActivityIndicator size="large" color="#0B646B" />
         </View>
-
-        <View>
-          <View className="flex-row items-center justify-between px-4 mt-8">
-            <Text className="text-[#2C7379] text-[28px] font-bold">
-              Top Tips
-            </Text>
-            <TouchableOpacity className="flex-row items-center justify-center space-x-1">
-              <Text className="text-[#A0C4C7] text-[20px] font-bold">
-                Explore
-              </Text>
-              <FontAwesome name="long-arrow-right" size={24} color="#A0C4C7" />
-            </TouchableOpacity>
-          </View>
-
-          <View className="flex-row items-center justify-evenly flex-wrap px-4 mt-8">
-            <ItemCardContainer
+      ) : (
+        <ScrollView>
+          <View className="flex-row items-center justify-between px-8 mt-8">
+            <MenuContainer
               key={"101"}
-              imageSrc={
-                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-              }
-              title="Something a very big"
-              location="Doha"
+              title="Hotels"
+              imageSrc={Hotels}
+              type={type}
+              setType={setType}
             />
-            <ItemCardContainer
+
+            <MenuContainer
               key={"102"}
-              imageSrc={
-                "https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg"
-              }
-              title="Simple"
-              location="Qatar"
+              title="Attractions"
+              imageSrc={Attractions}
+              type={type}
+              setType={setType}
+            />
+
+            <MenuContainer
+              key={"103"}
+              title="Restaurants"
+              imageSrc={Restaurants}
+              type={type}
+              setType={setType}
             />
           </View>
-        </View>
-      </ScrollView>
+
+          <View>
+            <View className="flex-row items-center justify-between px-4 mt-8">
+              <Text className="text-[#2C7379] text-[28px] font-bold">
+                Top Tips
+              </Text>
+              <TouchableOpacity className="flex-row items-center justify-center space-x-1">
+                <Text className="text-[#A0C4C7] text-[20px] font-bold">
+                  Explore
+                </Text>
+                <FontAwesome
+                  name="long-arrow-right"
+                  size={24}
+                  color="#A0C4C7"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View className="flex-row items-center justify-evenly flex-wrap px-4 mt-8">
+              {mainData?.length > 0 ? (
+                <>
+                  <ItemCardContainer
+                    key={"101"}
+                    imageSrc={
+                      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                    }
+                    title="Something a very big"
+                    location="Doha"
+                  />
+                  <ItemCardContainer
+                    key={"102"}
+                    imageSrc={
+                      "https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg"
+                    }
+                    title="Simple"
+                    location="Qatar"
+                  />
+                </>
+              ) : (
+                <>
+                  <View className="w-full h-[400px] items-center justify-center space-y-8">
+                    <Image
+                      source={NotFound}
+                      className="w-32 h-32 object-cover"
+                    />
+                    <Text className="text-2xl text-[#428288] font-semibold">
+                      Opps... No Data Found
+                    </Text>
+                  </View>
+                </>
+              )}
+            </View>
+          </View>
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
